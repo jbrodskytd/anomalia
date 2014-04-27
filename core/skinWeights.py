55079abdd4dde33915db126afe7150a9d3cc01b3
fleity <julianoberbeck@gmail.com>
2014-04-27 05:17:15 +0200

    skinweights v004
    works for multiple geometries, added function to rebind skinclusters

--- core/skinWeights.py
+++ core/skinWeights.py
@@ -7,28 +7,23 @@ import maya.OpenMayaAnim as oma
 from anomalia.core import common as common
 from anomalia.core.utils import showDialog
 
-def getSkinWeights( char = '', meshName = '' ):
+def getSkinWeights( char = '', meshes = [] ):
 	'''
+	saves skinweights of supplied or selected geometries to a file
+	the data is store in nested dictionaries in this structure
+	{geometry{vertexId{influenceId:weight}...}...}
 	'''
 
-	# check function requirements
 	# abort if no char is given
 	if char == '':
 		return cmds.warning('No character selected. Please supply a character name in function call.')
 
 	# abort if there no mesh supplied or selected
-	if meshName == '':
-		meshName = cmds.ls(sl=1)[0]
-		print( 'No mesh supplied as param, attempting to use from selected object(%s).' % meshName )
-		if meshName == []:
+	if meshes == []:
+		meshes = cmds.ls(sl=1)
+		print( 'No meshes supplied as param, attempting to use selected objects(%s).' % meshes )
+		if meshes == []:
 			return cmds.warning('No mesh to export weights from. Please select a mesh.')
-
-	# find skinCluster
-	clusterName = mel.eval('findRelatedSkinCluster '+ meshName)
-
-	# abort if there is no skinCluster
-	if clusterName == '':
-		return cmds.warning('No skinCluster found, mesh is not bound to a skeleton?')
 
 	#check if output file already exists
 	charDir = common.getCharDir( char )
@@ -37,87 +32,107 @@ def getSkinWeights( char = '', meshName = '' ):
 	if os.path.isfile(skinWeightsFile):
 		if cmds.confirmDialog( title = 'Warning', message = 'Overwrite existing skin weights data?', button = ['yes', 'no'] ) != 'yes':
 			return 'User cancelled'
-			
-	#prune to get rid of 0 zero value indices
-	normState = cmds.getAttr( '%s.normalizeWeights' % clusterName )
-	shapeName = cmds.listRelatives( meshName, shapes = True )[0]
-	cmds.setAttr( '%s.normalizeWeights' % clusterName, False )
-	cmds.skinPercent( clusterName, shapeName, normalize = False, pruneWeights = 0.01 )
-	cmds.setAttr('%s.normalizeWeights' % clusterName, normState )	
-	
-	# get the MFnSkinCluster for clusterName
-	selList = om.MSelectionList()
-	selList.add( clusterName )
-	clusterNode = om.MObject()
-	selList.getDependNode( 0, clusterNode )
-	skinFn = oma.MFnSkinCluster( clusterNode )
-	
-	# get the MDagPath for all influence
-	infDags = om.MDagPathArray()
-	skinFn.influenceObjects(infDags)
-	
-	# create a dictionary whose key is the MPlug indice id and 
-	# whose value is the influence list id
-	infIds = {}
-	infs = []
-	for i in xrange( infDags.length() ):
-		infPath = infDags[i].fullPathName()
-		infId = int(skinFn.indexForInfluenceObject( infDags[i] ) )
-		infIds[infId] = i
-		infs.append( infPath )
-	
-	# get weightList and weight plugs
-	weightListPlug = skinFn.findPlug('weightList')
-	weightsPlug = skinFn.findPlug('weights')
-	weightListAttr = weightListPlug.attribute()
-	weightsAttr = weightsPlug.attribute()
-	weightsInfIds = om.MIntArray()
-	
-	# the weights are stored in dictionary, the key is the vertId, 
-	# the value is another dictionary whose key is the influence id and 
-	# value is the weight for that influence
-	weights = {}
-	for vertId in xrange( weightListPlug.numElements() ):
-		vertWeights = {}
-		weightsPlug.selectAncestorLogicalIndex( vertId, weightListAttr )
+
+	allWeights = {}
+
+	for mesh in meshes:
+		# find skinCluster
+		clusterName = mel.eval('findRelatedSkinCluster '+ mesh)
+
+		# abort if there is no skinCluster
+		if clusterName == '':
+			cmds.warning('No skinCluster found, ' + mesh + ' is not bound to a skeleton?')
+			meshes.remove( mesh )
+			break
+
+		#prune to get rid of 0 zero value indices
+		normState = cmds.getAttr( '%s.normalizeWeights' % clusterName )
+		shapeName = cmds.listRelatives( mesh, shapes = True )[0]
+		cmds.setAttr( '%s.normalizeWeights' % clusterName, True )
+		cmds.skinPercent( clusterName, shapeName, normalize = False, pruneWeights = 0.01 )
+		cmds.setAttr('%s.normalizeWeights' % clusterName, normState )
+
+		# get the MFnSkinCluster for clusterName
+		selList = om.MSelectionList()
+		selList.add( clusterName )
+		clusterNode = om.MObject()
+		selList.getDependNode( 0, clusterNode )
+		skinFn = oma.MFnSkinCluster( clusterNode )
+
+		# get the MDagPath for all influence
+		infDags = om.MDagPathArray()
+		skinFn.influenceObjects(infDags)
+
+		# create a dictionary whose key is the MPlug indice id and
+		# whose value is the influence list id
+		infIds = {}
+		infs = []
+		for i in xrange( infDags.length() ):
+			infPath = infDags[i].fullPathName()
+			infId = int(skinFn.indexForInfluenceObject( infDags[i] ) )
+			infIds[infId] = i
+			infs.append( infPath )
+
+		# get weightList and weight plugs
+		weightListPlug = skinFn.findPlug('weightList')
+		weightsPlug = skinFn.findPlug('weights')
+		weightListAttr = weightListPlug.attribute()
+		weightsAttr = weightsPlug.attribute()
+		weightsInfIds = om.MIntArray()
+
+		# the weights are stored in dictionary, the key is the vertId,
+		# the value is another dictionary whose key is the influence id and
+		# value is the weight for that influence
+		# first key 'infs' is the list of influences in bind order
+		weights = {}	
+		weights['infs'] = infs
+		for vertId in xrange( weightListPlug.numElements() ):
+			vertWeights = {}
+			weightsPlug.selectAncestorLogicalIndex( vertId, weightListAttr )
+
+			weightsPlug.getExistingArrayAttributeIndices( weightsInfIds )
+
+			infPlug = om.MPlug( weightsPlug )
+			for infId in weightsInfIds:
+
+				infPlug.selectAncestorLogicalIndex( infId, weightsAttr )
+
+				try:
+					vertWeights[infIds[infId]] = infPlug.asDouble()
+				except KeyError:
+					pass
+
+			weights[vertId] = vertWeights
 		
-		weightsPlug.getExistingArrayAttributeIndices( weightsInfIds )
 		
-		infPlug = om.MPlug( weightsPlug )
-		for infId in weightsInfIds:
-			
-			infPlug.selectAncestorLogicalIndex( infId, weightsAttr )
-			
-			try:
-				vertWeights[infIds[infId]] = infPlug.asDouble()
-			except KeyError: 
-				pass
+		allWeights[mesh] = weights
+
 		
-		weights[vertId] = vertWeights		
-	
-	
 	#save data to file
 	f = open(skinWeightsFile, 'w')
-	f.write(json.dumps(weights))
+	f.write(json.dumps(allWeights))
 	f.close()
-	showDialog( 'Succss!', 'Skin weights saved to file:\n"%s_skin.py".' % skinWeightsFile)
+	showDialog( 'Succss!', 'Skin weights for [ %s ] saved to file:\n"%s_skin.py"' % (', '.join(meshes), skinWeightsFile) )
 	
-	return weights
+	return allWeights
 
 
-def setSkinWeights( char = '', meshName = '' ):
-
-	## check function requirements
+def setSkinWeights( char = '', meshes = [] ):
+	'''
+	applies skinweights from file to supplied or selected geometries
+	(which need to already have a skincluster)
+	'''
+	
 	# abort if no char is given
 	if char == '':
-		return cmds.warning('No character selected. Please supply a character name in function call.')
+		return cmds.warning('No character selected. Please supply a character name in function call.' )
 
 	# abort if there no mesh supplied or selected
-	if meshName == '':
-		meshName = cmds.ls(sl=1)[0]
-		print( 'No mesh supplied as param, attempting to use from selected object(%s).' % meshName )
-		if meshName == []:
-			return cmds.warning('No mesh to export weights from. Please select a mesh.')
+	if meshes == []:
+		meshes = cmds.ls(sl=1)
+		print( 'No meshes supplied as param, attempting to use selected objects(%s).' % ', '.join(meshes) )
+		if meshes == []:
+			return cmds.warning( 'No mesh to export weights from. Please select a mesh.' )
 
 	# get path to skin file
 	charDir = common.getCharDir( char )
@@ -126,39 +141,120 @@ def setSkinWeights( char = '', meshName = '' ):
 	# use default skinWeightsFile if character specific file does not exist
 	if not os.path.exists( skinWeightsFile ):
 		jointFile = os.path.join( common.getCharDir( 'defaultChar' ), 'defaultChar_joints.py' )
-		cmds.warning('Did not find skin weights file for selected character, using default character file.')
+		cmds.warning( 'Did not find skin weights file for selected character, using default character file.' )
 
 	# read skin weights from file
 	f = open( skinWeightsFile, 'r' )
-	weights = json.loads( f.readline() )
+	allWeights = json.loads( f.readline() )
 	f.close()
+	
+	# compare meshes to keys of allWeights dictionary
+	print( 'File contains skinweights for: ' + ','.join( allWeights.keys() ) )	
+	for mesh in meshes:
+		if mesh not in allWeights.keys():
+			cmds.warning( 'No weights found for %s' % mesh )
+			meshes.remove( mesh )
 
-	# find skinCluster 
-	clusterName = mel.eval('findRelatedSkinCluster '+ meshName)
+	for mesh in meshes:
+				
+		# find skinCluster
+		clusterName = mel.eval('findRelatedSkinCluster '+ mesh)
+
+		if clusterName == '':
+			cmds.warning('No skinclutser found on %s.' % mesh)
+			break
+
+		weights = allWeights[mesh]
 	
-	# unlock influences	
-	infs = cmds.skinCluster( clusterName, q = True, weightedInfluence = True )
-	for inf in infs:
-		cmds.setAttr( '%s.liw' %inf, False )
+		# unlock influences	
+		infs = cmds.skinCluster( clusterName, q = True, weightedInfluence = True )
+		for inf in infs:
+			cmds.setAttr( '%s.liw' %inf, False )
+			
+		# prune weights to get rid of weights not stored in skinWeightsFile
+		# normalize needs to be turned off for the prune to work
+		normState = cmds.getAttr( '%s.normalizeWeights' % clusterName )
+		shapeName = cmds.listRelatives( mesh, shapes = True )[0]
+		cmds.setAttr( '%s.normalizeWeights' % clusterName, False )
+		cmds.skinPercent( clusterName, shapeName, normalize = False, pruneWeights = 100.0 )
+		cmds.setAttr('%s.normalizeWeights' % clusterName, normState )
 		
-	# prune weights to get rid of weights not stored in skinWeightsFile
-	# normalize needs to be turned off for the prune to work
-	normState = cmds.getAttr( '%s.normalizeWeights' % clusterName )
-	shapeName = cmds.listRelatives( meshName, shapes = True )[0]
-	cmds.setAttr( '%s.normalizeWeights' % clusterName, False )
-	cmds.skinPercent( clusterName, shapeName, normalize = False, pruneWeights = 100.0 )
-	cmds.setAttr('%s.normalizeWeights' % clusterName, normState )
+		# pop influences key
+		if 'infs' in weights: weights.pop('infs')
+		
+		# set weights in skinCluster.weightsList[].weights[] to infValue
+		for vertId, weightData in weights.items():
+			weightListAttr = '%s.weightList[%s]' % ( clusterName, vertId )
+			for infId, infValue in weightData.items():
+				weightsAttr = '.weights[%s]' % infId
+				cmds.setAttr( weightListAttr + weightsAttr, infValue )
 	
-	# set weights in skinCluster.weightsList[].weights[] to infValue
-	for vertId, weightData in weights.items():
-		weightListAttr = '%s.weightList[%s]' % ( clusterName, vertId )
-		for infId, infValue in weightData.items():
-			weightsAttr = '.weights[%s]' % infId
-			cmds.setAttr( weightListAttr + weightsAttr, infValue )
-	
-	showDialog( 'Succss!', 'Skin weights applied to %s from file:\n"%s_skin.py".' % ( meshName, skinWeightsFile) )
+	showDialog( 'Succss!', 'Skin weights applied to [ %s ] from file:\n"%s_skin.py"' % ( ', '.join(meshes), skinWeightsFile) )
 	
 	return
+	
+def rebindSkinClusters( char = '', meshes = [] ):
+	'''
+	delete and recreate skinclusters on supplied or selected meshes
+	with influences from skinWeightsFile
+	'''
 
-#getSkinWeights( char = 'char2' )
-#setSkinWeights( char = 'char2' )
+	# abort if no char is given
+	if char == '':
+		return cmds.warning('No character selected. Please supply a character name in function call.' )
+
+	# abort if there no mesh supplied or selected
+	if meshes == []:
+		meshes = cmds.ls(sl=1)
+		print( 'No meshes supplied as param, attempting to use selected objects(%s).' % ', '.join(meshes) )
+		if meshes == []:
+			return cmds.warning( 'No mesh to found. Please select a mesh.' )
+
+	# confirm rebind
+	if cmds.confirmDialog( title = 'Warning', message = 'Delete and rebind skinclusters for [ %s ]?' % ', '.join(meshes), button = ['yes', 'no'] ) != 'yes':
+		return 'User cancelled'
+
+	# get path to skin file
+	charDir = common.getCharDir( char )
+	skinWeightsFile = os.path.join( charDir, '%s_skin.py' % char )
+
+	# use default skinWeightsFile if character specific file does not exist
+	if not os.path.exists( skinWeightsFile ):
+		jointFile = os.path.join( common.getCharDir( 'defaultChar' ), 'defaultChar_joints.py' )
+		cmds.warning( 'Did not find skin weights file for selected character, using default character file.' )
+
+	# read skin weights from file
+	f = open( skinWeightsFile, 'r' )
+	allWeights = json.loads( f.readline() )
+	f.close()
+
+	# remove meshes for which there is no infs-key or weight dictionary
+	for mesh in meshes:
+		if mesh not in allWeights.keys():
+			cmds.warning( 'No weights dictionary found for %s' % mesh )
+			meshes.remove( mesh )
+		if 'infs' not in allWeights[mesh].keys():
+			cmds.warning( 'No Influences key found for %s' % mesh )
+			meshes.remove( mesh )
+
+	skinclusters = []
+	for mesh in meshes:
+		weights = allWeights[mesh]
+		
+		# unbind mesh if skincluster exists
+		clusterName = mel.eval('findRelatedSkinCluster '+ mesh)
+		if clusterName != '': cmds.skinCluster( mesh, edit = True, unbind = True )
+		
+		# rebind
+		skincluster = cmds.skinCluster( weights['infs'], mesh, name = 'skincluster_' + str( mesh ), toSelectedBones = True, maximumInfluences = 3, obeyMaxInfluences = True, skinMethod = 0  )
+		skinclusters.append( skincluster )
+	
+	showDialog( 'Succss!', 'Skinclusters rebound on [ %s ] from file:\n"%s_skin.py"' % ( ', '.join(meshes), skinWeightsFile) )
+	cmds.select( meshes )
+	
+	return skinclusters
+
+
+#getSkinWeights( 'char' )
+#setSkinWeights( 'char' )
+#rebindSkinClusters( 'char' )
