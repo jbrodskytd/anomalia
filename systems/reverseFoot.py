fd4e8334e571833f745228cd266648aaaedcfd91
fleity <julianoberbeck@gmail.com>
2014-05-10 02:30:01 +0200

    reverse foot v003
    creates lattice, ground ctrl, no fake collision setup yet

--- systems/reverseFoot.py
+++ systems/reverseFoot.py
@@ -10,14 +10,32 @@ def build( side = None, jntFoot = None, ctrlFoot = None, mesh = None ):
 	If a mesh called 'body' is in the scene or supplied function tries
 	to use a specific set of vertices as positions for the roll pivots.
 	
-	returns a dictionary containing with these keys: joints, pivots, ikHandles, ctrl
+	returns a dictionary containing corresponding objects for these keys: 
+	joints, pivots, ikHandles, lattice, ctrls
 	 
-	to do: lattice
+	to do: lattice ground contact setup
 	'''
 	
-	# abort if no side is specified
-	if side not in ['lf', 'left', 'lft', 'rt', 'right', 'rgt']:
-		return pm.warning( 'No side specified.' )	
+	
+	# vertex indices for left / right
+	pivotVertsLeft = [196, 199, 29, 28]
+	latticeVertIdsLeft = ['0:1', '8:9', '28:29', '39:42', '47:48', '51:52', '55:58', '196', '199', '206:207', '211:212', '241:242', '253', '264:265', '268:273', '278:279', '282:285', '290:291', '409', '414', '418', '434', '437:438', '447:448']	
+	
+	pivotVertsRight = [219, 230, 36, 35]
+	latticeVertIdsRight = ['14:15', '24:25', '35:36', '43:46', '49:50', '53:54', '59:62', '219', '230:232', '237:238', '248:249', '258', '261:262', '266:267', '274:277', '280:281', '286:289', '292:293', '424:426', '430:431', '441:443']
+
+
+	# set correct vertices for left or right, abort if not side specified
+	if side in ['lf', 'left', 'lft']:
+		pivotVerts = pivotVertsLeft
+		latticeVertIds = latticeVertIdsLeft
+	elif side in ['rt', 'right', 'rgt']:
+		pivotVerts = pivotVertsRight
+		latticeVertIds = latticeVertIdsRight
+		
+	else:
+		return cmds.warning( 'No valid side specified' )
+
 		
 	# if footJnt is not specified try to use selection
 	if jntFoot == None:
@@ -40,17 +58,17 @@ def build( side = None, jntFoot = None, ctrlFoot = None, mesh = None ):
 	outDict = {}
 	
 	# create ctrl
-	footCtrl = controls.Control( side = side, rigPart = "foot", function = "ik", nodeType = "ctrl", size = .75, color = "yellow", aimAxis = "x" )
-	footCtrl.circleCtrl()
-	footGrp = common.insertGroup( footCtrl.control )
-	common.align( footGrp, jntFoot, translate = True, orient = True )
-	outDict['ctrl'] = footCtrl.control
+	ctrlFoot = controls.Control( side = side, rigPart = "foot", function = "ik", nodeType = "ctrl", size = .65, color = "yellow", aimAxis = "x" )
+	ctrlFoot.cubeCtrl()
+	footGrp = common.insertGroup( ctrlFoot.control )
+	common.align( footGrp, jntFoot, translate = True, orient = False )
+	outDict['ctrl'] = ctrlFoot.control
 	
 	
 	# add attributes	
-	attrs = ['roll', 'side', 'toeTap', 'toeRaise', 'toeTwist', 'heelTwist']
+	attrs = ['roll', 'side', 'toeTap', 'toeRaise', 'toeTwist', 'heelTwist', 'squashGroundContact', 'showGroundControl']
 	for attr in attrs:
-		cmds.addAttr( footCtrl.control, longName=attr, attributeType='float', keyable = True )
+		cmds.addAttr( ctrlFoot.control, longName=attr, attributeType='float', keyable = True )
 	
 	jntFootPos = cmds.xform( jntFoot, q = True, translation = True, ws = True )
 	
@@ -66,15 +84,7 @@ def build( side = None, jntFoot = None, ctrlFoot = None, mesh = None ):
 		pivotRightPos = [ jntFootPos[0] - pivotOffset, jntFootPos[1], jntFootPos[2] ]
 		
 	else:
-		
-		# set correct vertices for left or right
-		if side in ['lf', 'left', 'lft']:
-			pivotVerts = [196, 199, 29, 28] 
-		elif side in ['rt', 'right', 'rgt']:
-			pivotVerts = [219, 230, 36, 35]
-		else:
-			return cmds.warning( 'No valid side specified' )
-		
+				
 		# get pivot positions from vertices
 		pivotFrontPos = cmds.xform( 'body.vtx[%s]' % pivotVerts[0], q = True, t = True )
 		pivotRearPos = cmds.xform( 'body.vtx[%s]' % pivotVerts[1], q = True, t = True )
@@ -127,49 +137,81 @@ def build( side = None, jntFoot = None, ctrlFoot = None, mesh = None ):
 	clampRollFront = cmds.shadingNode( 'clamp', asUtility = True )
 	clampRollFront = cmds.rename( clampRollFront, common.getName( node=clampRollFront, side=side, rigPart='front', function='roll', nodeType='clamp') )
 	cmds.setAttr( clampRollFront + '.maxR', limitRoll )
-	cmds.connectAttr( footCtrl.control + '.roll', clampRollFront + '.inputR' )
+	cmds.connectAttr( ctrlFoot.control + '.roll', clampRollFront + '.inputR' )
 	cmds.connectAttr( clampRollFront + '.outputR', pivotFront + '.rotateX' )
 	
 	clampRollRear = cmds.shadingNode( 'clamp', asUtility = True )
 	clampRollRear = cmds.rename( clampRollRear, common.getName( node=clampRollRear, side=side, rigPart='rear', function='roll', nodeType='clamp') )
 	cmds.setAttr( clampRollRear + '.minR', -limitRoll )
-	cmds.connectAttr( footCtrl.control + '.roll', clampRollRear + '.inputR' )
+	cmds.connectAttr( ctrlFoot.control + '.roll', clampRollRear + '.inputR' )
 	cmds.connectAttr( clampRollRear + '.outputR', pivotRear + '.rotateX' )
 
 	clampRollLeft = cmds.shadingNode( 'clamp', asUtility = True )
 	clampRollLeft = cmds.rename( clampRollLeft, common.getName( node=clampRollLeft, side=side, rigPart='left', function='side', nodeType='clamp') )
 	cmds.setAttr( clampRollLeft + '.minR', -limitRoll )
-	cmds.connectAttr( footCtrl.control + '.side', clampRollLeft + '.inputR' )
+	cmds.connectAttr( ctrlFoot.control + '.side', clampRollLeft + '.inputR' )
 	cmds.connectAttr( clampRollLeft + '.outputR', pivotLeft + '.rotateZ' )
 	
 	clampRollRight = cmds.shadingNode( 'clamp', asUtility = True )
 	clampRollRight = cmds.rename( clampRollRight, common.getName( node=clampRollRight, side=side, rigPart='right', function='side', nodeType='clamp') )
 	cmds.setAttr( clampRollRight + '.maxR', limitRoll )
-	cmds.connectAttr( footCtrl.control + '.side', clampRollRight + '.inputR' )
+	cmds.connectAttr( ctrlFoot.control + '.side', clampRollRight + '.inputR' )
 	cmds.connectAttr( clampRollRight + '.outputR', pivotRight + '.rotateZ' )	
 	
 	
 	# connect twist attributes
-	cmds.connectAttr( footCtrl.control + '.toeTwist', pivotFront + '.rotateY' )
-	cmds.connectAttr( footCtrl.control + '.heelTwist', pivotRear + '.rotateY' )
+	cmds.connectAttr( ctrlFoot.control + '.toeTwist', pivotFront + '.rotateY' )
+	cmds.connectAttr( ctrlFoot.control + '.heelTwist', pivotRear + '.rotateY' )
 
 	# create pivot for toe raise, parent ball ikHandle and connect
 	pivToeRaise = common.insertGroup( pivotFront )
 	pivToeRaise = cmds.rename( pivToeRaise, common.getName( node=pivToeRaise, side=side, rigPart='toe', function='raise_piv', nodeType='grp') )
 	cmds.parent( ikHandleBall, pivToeRaise )
-	cmds.connectAttr( footCtrl.control + '.toeRaise', pivToeRaise + '.rotateX' )
+	cmds.connectAttr( ctrlFoot.control + '.toeRaise', pivToeRaise + '.rotateX' )
 	
 	# create pivot for toe tap and connect
 	pivToeTap = cmds.group( ikHandleToe )
 	pivToeTap = cmds.rename( pivToeTap, common.getName( node=pivToeTap, side=side, rigPart='toe', function='tap_piv', nodeType='grp') )
 	cmds.xform( pivToeTap, preserve = True, ws = True, piv = cmds.xform( jntBall, q = True, translation = True, ws = True) )
-	cmds.connectAttr( footCtrl.control + '.toeTap', pivToeTap + '.rotateX' )
+	cmds.connectAttr( ctrlFoot.control + '.toeTap', pivToeTap + '.rotateX' )
+	
+
+	# create lattice deformer	
+	latticeVerts = []
+	for vertId in latticeVertIds:
+		latticeVerts.append( '%s.vtx[%s]' % (mesh, vertId) )
+	cmds.select( latticeVerts )
+	
+	ffd, ffdLattice, ffdBase = cmds.lattice( divisions =(3, 3, 3), objectCentered = True, outsideLattice = 2, ofd = 0.25 )
+	ffd = cmds.rename( ffd, common.getName( node=ffd, side=side, rigPart='foot', function='contact', nodeType='ffd') )
+	ffdLattice = cmds.rename( ffdLattice, common.getName( node=ffdLattice, side=side, rigPart='foot', function='contact', nodeType='ffdlattice') )
+	ffdBase = cmds.rename( ffdBase, common.getName( node=ffdBase, side=side, rigPart='foot', function='contact', nodeType='ffdbase') )	
+	cmds.connectAttr( ctrlFoot.control + '.squashGroundContact', ffd + '.envelope' )
+	cmds.addAttr( ctrlFoot.control + '.squashGroundContact', edit = True, max = 1.00, min = 0.00 )
+	
+	outDict['lattice'] = ffd, ffdLattice, ffdBase
+	
+	# set up contact ctrl for lattice
+	ctrlGround = controls.Control( side = 'rt', rigPart = "foot", function = "contact", nodeType = "ctrl", size = 1.1, color = "red", aimAxis = "x" )
+	ctrlGround.squareCtrl()
+	cmds.rotate( 0, 45, 0, ctrlGround.control + '.cv[0:4]', relative = True, objectSpace = True )
+	cmds.addAttr( ctrlFoot.control, longName = 'showGroundCtrl', attributeType = 'bool', keyable = True )
+	cmds.connectAttr( ctrlFoot.control + '.showGroundCtrl', ctrlGround.control + '.visibility' )
+	cmds.setAttr( ctrlGround.control + '.scaleX', lock = True, keyable = False, channelBox = False )
+	cmds.setAttr( ctrlGround.control + '.scaleY', lock = True, keyable = False, channelBox = False )
+	cmds.setAttr( ctrlGround.control + '.scaleZ', lock = True, keyable = False, channelBox = False )
+	cmds.setAttr( ctrlGround.control + '.visibility', keyable = False, channelBox = False )
+	cmds.xform( ctrlGround.control, ws = True, t = cmds.xform( ffdLattice + '.pt[1][0][1]', q = True, ws = True , t = True) )
+	cmds.parent( ctrlGround.control, ctrlFoot.control )
+	common.insertGroup( ctrlGround.control )
+	
 	
 	
 	####remove this
-	ikHandleLeg, ikEffectorLeg = cmds.ikHandle( startJoint = 'lf_upLeg_def_jnt', endEffector = 'lf_foot_def_jnt', solver = 'ikRPsolver', name = 'ikHandle_leg')
+	ikHandleLeg, ikEffectorLeg = cmds.ikHandle( startJoint = side + '_upLeg_def_jnt', endEffector = side + '_foot_def_jnt', solver = 'ikRPsolver', name = side + '_ikHandle_leg')
 	cmds.parent( ikHandleLeg, pivotFront )
+	cmds.parent( pivotRight, ctrlFoot.control )
+	cmds.parent( ffdLattice, ffdBase, ctrlFoot.control )
 	
-	
-	cmds.select( footCtrl.control )
+	cmds.select( ctrlFoot.control )
 	return outDict
