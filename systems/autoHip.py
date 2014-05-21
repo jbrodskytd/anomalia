e5c65320993eaae96fdd72fcb3b1151d6bee9b08
Davis3d <davis.3dgrafik@gmail.com>
2014-05-21 13:58:14 +0200

    Update autoHip.py
    Correction when the pelvisCTRL is moved, now autoHip moves fully with pelvisCTRL. Now there is limitation, that pelvisCTRL should be zeroed, to have autoHip moving correctly.

--- systems/autoHip.py
+++ systems/autoHip.py
@@ -17,12 +17,22 @@ def createAutoHip(leg_jnt1, pelvis_ctrl, foot_ctrl):
     cmds.setAttr(placer[0]+".translate",0,0,0)
     cmds.parent(placer[0], pelvis_ctrl)
     
+   
+    name = '%s_%s_%s_%s' % ( common.getSide(leg_jnt1), 'footCtrl', 'constraint', 'grp' )
+    
+    placerFootGrp = cmds.group(n=name, em = True)
+    cmds.parent(placerFootGrp, foot_ctrl)
+    cmds.setAttr(placerFootGrp+".translate",0,0,0)
+    cmds.parent(placerFootGrp, pelvis_ctrl)
+   
     name = '%s_%s_%s_%s' % ( common.getSide(leg_jnt1), "footCtrl", 'constraint', 'loc' )    
     placerFoot = cmds.spaceLocator(n=name)
     cmds.parent(placerFoot[0], leg_jnt1)
     cmds.setAttr(placerFoot[0]+".translate",0,0,0)
-    cmds.parent(placerFoot[0], foot_ctrl)
+    cmds.parent(placerFoot[0], placerFootGrp)
     
+    placer_footCtrl_point = cmds.pointConstraint(foot_ctrl, placerFootGrp, w=1, mo = False)
+    cmds.connectAttr(pelvis_ctrl+".translate",placer_footCtrl_point[0]+".offset")
 
     name = '%s_%s_%s_%s' % ( common.getSide(leg_jnt1), 'hip', 'autoHip', 'ctrl' )    
     hipCtrlObj = controls.Control( side = common.getSide(leg_jnt1), rigPart = "hip", function = "autoHip", nodeType = "ctrl", size = .4, color = "yellow", aimAxis = "z" ) #cmds.circle(r=.3, n=name) #TODO: create proper controller, positioning
