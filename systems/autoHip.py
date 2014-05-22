6cc11c645643dcd92cbd3cd124c2625376ab0543
Davis3d <davis.3dgrafik@gmail.com>
2014-05-22 12:41:25 +0200

    Update autoHip.py
    autoHip now works with pelvisCtrl position not zeroed, hipCtrl has separated XYZ weights and hipCtrl has now proper color.

--- systems/autoHip.py
+++ systems/autoHip.py
@@ -32,10 +32,14 @@ def createAutoHip(leg_jnt1, pelvis_ctrl, foot_ctrl):
     cmds.parent(placerFoot[0], placerFootGrp)
     
     placer_footCtrl_point = cmds.pointConstraint(foot_ctrl, placerFootGrp, w=1, mo = False)
-    cmds.connectAttr(pelvis_ctrl+".translate",placer_footCtrl_point[0]+".offset")
+    
 
-    name = '%s_%s_%s_%s' % ( common.getSide(leg_jnt1), 'hip', 'autoHip', 'ctrl' )    
-    hipCtrlObj = controls.Control( side = common.getSide(leg_jnt1), rigPart = "hip", function = "autoHip", nodeType = "ctrl", size = .4, color = "yellow", aimAxis = "z" ) #cmds.circle(r=.3, n=name) #TODO: create proper controller, positioning
+    name = '%s_%s_%s_%s' % ( common.getSide(leg_jnt1), 'hip', 'autoHip', 'ctrl' )
+    if common.getSide(leg_jnt1) == 'rt':
+        col = 'red'
+    else:
+        col = 'blue'
+    hipCtrlObj = controls.Control( side = common.getSide(leg_jnt1), rigPart = "hip", function = "autoHip", nodeType = "ctrl", size = .4, color = col, aimAxis = "z" ) #cmds.circle(r=.3, n=name) #TODO: create proper controller, positioning
     hipCtrlObj.controlName = name
     hipCtrlObj.pinCtrl()
     
@@ -66,25 +70,52 @@ def createAutoHip(leg_jnt1, pelvis_ctrl, foot_ctrl):
     cmds.setAttr(hipGrp+".translate", tempPos[0], tempPos[1], tempPos[2])
 
 
-    pelvis_autoHip_parent = cmds.parentConstraint(pelvis_ctrl, hipGrp, mo = True)
-    cmds.parentConstraint(hipCtrl, leg_jnt1, mo = True)
+    pelvis_autoHip_parent = cmds.parentConstraint(pelvis_ctrl, hipGrp, mo = False)
+    cmds.parentConstraint(hipCtrl, leg_jnt1, mo = False)
 
 
-    placer_hipNull_point = cmds.pointConstraint(placer, placerFoot, hipNull, w=0.5, mo = False)
+    placer_hipNull_parentX = cmds.parentConstraint(placer, placerFoot, hipNull, w=0.5, mo = False, st = ['y','z'], sr = ['x','y','z'])
+    placer_hipNull_parentY = cmds.parentConstraint(placer, placerFoot, hipNull, w=0.5, mo = False, st = ['x','z'], sr = ['x','y','z'])
+    placer_hipNull_parentZ = cmds.parentConstraint(placer, placerFoot, hipNull, w=0.5, mo = False, st = ['x','y'], sr = ['x','y','z'])
 
     pma = cmds.createNode("plusMinusAverage")
     
-    cmds.addAttr(hipCtrl, at='float', ln='weight', dv=0.5, min=0, h=False, k=True)
-    
     cmds.setAttr(pma + '.operation', 2)
-    cmds.setAttr(pma + '.input1D[0]', 1)
+    cmds.setAttr(pma + '.input3D[0]', 1,1,1, type="float3")
     
-    cmds.connectAttr(hipCtrl + '.weight', placer_hipNull_point[0] + '.' + placer[0] + 'W0')
-    cmds.connectAttr(placer_hipNull_point[0] + '.' + placer[0] + 'W0', pma+ '.input1D[1]')
-    cmds.connectAttr(pma+'.output1D', placer_hipNull_point[0] + '.' + placerFoot[0] + 'W1')
+    pma2 = cmds.createNode("plusMinusAverage")
+    cmds.setAttr(pma2 + '.operation', 2)
+    cmds.connectAttr(pelvis_ctrl + '.translate', pma2 + '.input3D[0]')
+    cmds.setAttr(pma2 + '.input3D[1].input3Dx', cmds.getAttr(pelvis_ctrl + '.translateX'))
+    cmds.setAttr(pma2 + '.input3D[1].input3Dy', cmds.getAttr(pelvis_ctrl + '.translateY'))
+    cmds.setAttr(pma2 + '.input3D[1].input3Dz', cmds.getAttr(pelvis_ctrl + '.translateZ'))
+    
+    mp = cmds.createNode("multiplyDivide")
+    cmds.setAttr(mp + ".operation", 1)
+    
+    cmds.addAttr(hipCtrl, at='float', ln='weightX', dv=0.5, min=0, h=False, k=True)
+    cmds.addAttr(hipCtrl, at='float', ln='weightY', dv=0.5, min=0, h=False, k=True)
+    cmds.addAttr(hipCtrl, at='float', ln='weightZ', dv=0.5, min=0, h=False, k=True)
+    
+    
+    cmds.connectAttr(pma+'.output3D.output3Dx', placer_hipNull_parentX[0] + '.' + placer[0] + 'W0')
+    cmds.connectAttr(pma+'.output3D.output3Dy', placer_hipNull_parentY[0] + '.' + placer[0] + 'W0')
+    cmds.connectAttr(pma+'.output3D.output3Dz', placer_hipNull_parentZ[0] + '.' + placer[0] + 'W0')
+
+    cmds.connectAttr(pma2 + '.output3D', mp + '.input1')
+    cmds.setAttr(mp + '.input2',2,2,2, type = 'float3')
+    cmds.connectAttr(mp+".output",placer_footCtrl_point[0]+".offset")
+    
+    cmds.connectAttr(placer_hipNull_parentX[0] + '.' + placerFoot[0] + 'W1', pma+ '.input3D[1].input3Dx')
+    cmds.connectAttr(placer_hipNull_parentY[0] + '.' + placerFoot[0] + 'W1', pma+ '.input3D[1].input3Dy')
+    cmds.connectAttr(placer_hipNull_parentZ[0] + '.' + placerFoot[0] + 'W1', pma+ '.input3D[1].input3Dz')
+    
+    cmds.connectAttr(hipCtrl + '.weightX', placer_hipNull_parentX[0] + '.' + placerFoot[0] + 'W1')
+    cmds.connectAttr(hipCtrl + '.weightY', placer_hipNull_parentY[0] + '.' + placerFoot[0] + 'W1')
+    cmds.connectAttr(hipCtrl + '.weightZ', placer_hipNull_parentZ[0] + '.' + placerFoot[0] + 'W1')
 
     
    
 def test():
-    createAutoHip("rt_leg_skin_jnt", "cn_cog_fk_ctrl", "lf_foot_ik_ctrl")
-    
+    createAutoHip("lf_leg_skin_jnt", "cn_cog_fk_ctrl", "lf_foot_ik_ctrl")
+
